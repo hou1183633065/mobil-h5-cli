a7b79a934b12f640a17dd641dc47021fb76dfeaf
houzhiqiang <1183633065@qq.com>
2020-04-24 14:04:21 +0800

    添加log/debug的移除，添加操作符的支持

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,23 @@
+let plugins = [
+  [
+    'import',
+    {
+      libraryName: 'vant',
+      libraryDirectory: 'es',
+      style: true
+    },
+    'vant'
+  ],
+  '@babel/plugin-proposal-optional-chaining', // 支持?.可选链操作符
+  '@babel/plugin-proposal-nullish-coalescing-operator' // 支持??空值合并运算符
+];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push('transform-remove-console'); // 移除console
+  plugins.push('transform-remove-debugger'); // 移除的bugger
+}
+
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset'],
-  plugins: [
-    [
-      'import',
-      {
-        libraryName: 'vant',
-        libraryDirectory: 'es',
-        style: true
-      },
-      'vant'
-    ]
-  ]
+  plugins
 };
