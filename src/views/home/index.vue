0647fe81d01d4ef4d4e72a22bbf0bab73061502e
houzhiqiang <1183633065@qq.com>
2020-04-26 15:40:07 +0800

    加入样式生成器

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <div class="test-box1">
-      <div class="test-box2">this is content</div>
+      <div class="test-box2 fs-30">this is content</div>
     </div>
     <!-- <van-button type="primary" size="large">大号按钮</van-button>
     <van-button type="primary" size="normal">普通按钮</van-button>
